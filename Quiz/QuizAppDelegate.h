82220c08e5d73c316c63efefdc3764b708f92926
gmarik <gmarik@gmail.com>
2011-07-17 16:47:12 -0500

    Initial Commit

---
+++ Quiz/QuizAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  QuizAppDelegate.h
+//  Quiz
+//
+//  Created by Marjan Hratson on 7/17/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
