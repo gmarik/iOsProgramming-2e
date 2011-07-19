3a5a341eb611007fed48d81e1e28b285dd93b4e5
gmarik <gmarik@gmail.com>
2011-07-19 00:41:02 -0500

    02 implementation

--- 02-RandomPossessions/RandomPossessions/RandomPossessions/main.m
+++ 02-RandomPossessions/RandomPossessions/RandomPossessions/main.m
@@ -13,8 +13,29 @@ int main (int argc, const char * argv[])
 
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-    // insert code here...
-    NSLog(@"Hello, World!");
+    // allocate and init items
+    NSMutableArray *items = [[NSMutableArray alloc] init];
+    
+    [items addObject:@"One"];
+    [items addObject:@"Two"];
+    [items addObject:@"Tre"];
+    
+    [items insertObject:@"Zero" atIndex:0];
+    
+    for(int i = 0; i < [items count]; i+=1) {
+        NSLog(@"%@", [items objectAtIndex:i]);
+        // %@ is a shorthand for
+        // NSLog(@"%s", [[items objectAtIndex:i] description]);
+        // which is kind of Object#to_s or Object#inspect in Ruby
+
+    }
+    
+    //release
+    [items release];
+    // Don't leave items pointint at freed memory!
+    items = nil;
+    // sending messages to nil is isgnored
+    // sending messages to "released object" causes issues 
 
     [pool drain];
     return 0;
