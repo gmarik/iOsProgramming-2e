5d2dff2e355e3e005227ca9bb6bd95458b985e93
gmarik <gmarik@gmail.com>
2011-07-21 21:23:52 -0500

    02 initial Posession class

---
+++ 02-RandomPossessions/RandomPossessions/RandomPossessions/Posession.h
@@ -0,0 +1,16 @@
+//
+//  Posession.h
+//  RandomPossessions
+//
+//  Created by Marjan Hratson on 7/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Posession : NSObject {
+    
+}
+
+@end
---
+++ 02-RandomPossessions/RandomPossessions/RandomPossessions/Posession.m
@@ -0,0 +1,14 @@
+//
+//  Posession.m
+//  RandomPossessions
+//
+//  Created by Marjan Hratson on 7/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Posession.h"
+
+
+@implementation Posession
+
+@end
