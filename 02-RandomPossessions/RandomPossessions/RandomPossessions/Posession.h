df98e7ac81b61b57b948f3c5dcdd07c437e6f94c
gmarik <gmarik@gmail.com>
2011-07-21 21:43:19 -0500

    02 Posession interface & implementation

--- 02-RandomPossessions/RandomPossessions/RandomPossessions/Posession.h
+++ 02-RandomPossessions/RandomPossessions/RandomPossessions/Posession.h
@@ -10,7 +10,24 @@
 
 
 @interface Posession : NSObject {
+
+    NSString *posessionName;
+    NSString *serialNumber;
+    int valueInDollars;
+    NSDate *dateCreated;
     
 }
+
+-(NSString *)posessionName;
+-(void)setPosessionName:(NSString *)newValue;
+
+-(NSString *) serialNumber;
+-(void)setSerialNumber:(NSString *)newValue;
+
+-(NSDate *) dateCreated;
+-(void)setDateCreated:(NSDate *)newValue;
+
+-(int) valueInDollars;
+-(void)setValueInDollars:(int)newValue;
 
 @end
--- 02-RandomPossessions/RandomPossessions/RandomPossessions/Posession.m
+++ 02-RandomPossessions/RandomPossessions/RandomPossessions/Posession.m
@@ -11,4 +11,32 @@
 
 @implementation Posession
 
+- (NSString *)posessionName {
+    return posessionName;
+}
+-(void)setPosessionName:(NSString *)newValue {
+    posessionName = newValue;
+}
+
+- (NSString *)serialNumber {
+    return serialNumber;
+}
+- (void)setSerialNumber:(NSString *)newValue {
+    serialNumber = newValue;
+}
+
+- (NSDate *)dateCreated {
+    return dateCreated;
+}
+- (void)setDateCreated:(NSDate *)newValue {
+    dateCreated = newValue;
+}
+
+- (int)valueInDollars {
+    return valueInDollars;
+}
+- (void)setValueInDollars:(int)newValue {
+    valueInDollars = newValue;
+}
+
 @end
