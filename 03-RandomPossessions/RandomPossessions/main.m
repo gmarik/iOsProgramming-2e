75240e623a82ebc70e18febe5a805ff26da00edf
gmarik <gmarik@gmail.com>
2011-07-23 20:26:02 -0500

    03 initial code (copy & mod from ch 02)

---
+++ 03-RandomPossessions/RandomPossessions/main.m
@@ -0,0 +1,35 @@
+//
+//  main.m
+//  RandomPossessions
+//
+//  Created by Marjan Hratson on 7/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Posession.h"
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // allocate and init items
+    NSMutableArray *items = [[NSMutableArray alloc] init];
+    
+    [items addObject:[Posession randomPosession]];
+    [items addObject:[Posession randomPosession]];
+    [items addObject:[Posession randomPosession]];
+    
+    [items insertObject:[Posession randomPosession] atIndex:0];
+    
+    for(NSString *item in items) {
+        NSLog(@"%@", item);
+    }
+    
+    //release
+    [items release];
+    items = nil;
+
+    [pool drain];
+    return 0;
+}
