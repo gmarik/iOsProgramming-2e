a5ac7636221a0c36e94ceb091583844e8758389c
gmarik <gmarik@gmail.com>
2011-07-28 00:05:52 -0500

    04 Initial boilerplate

---
+++ 04-DelegationAndCoreLocation/Whereami/WhereamiAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  WhereamiAppDelegate.h
+//  Whereami
+//
+//  Created by Marjan Hratson on 7/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhereamiAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
