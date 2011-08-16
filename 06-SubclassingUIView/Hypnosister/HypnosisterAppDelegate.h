551ce5e68b18cdf932c5715223039a43904b158f
gmarik <gmarik@gmail.com>
2011-08-16 00:05:22 -0500

    06 boilerplate

---
+++ 06-SubclassingUIView/Hypnosister/HypnosisterAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by Marjan Hratson on 8/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
