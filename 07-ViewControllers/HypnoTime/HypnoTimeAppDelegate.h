eb26b467d90ac6e5a7e0c9c5abb1c3049aa95501
gmarik <gmarik@gmail.com>
2011-08-29 20:22:14 -0500

    07 ViewControllers
    - HypnoTime initial import

---
+++ 07-ViewControllers/HypnoTime/HypnoTimeAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HypnoTimeAppDelegate.h
+//  HypnoTime
+//
+//  Created by Marjan Hratson on 8/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnoTimeAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
