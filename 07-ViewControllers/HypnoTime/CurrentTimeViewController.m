68fe93a0282f4bcdeb46eff6784eb2f3a7d4ff2b
gmarik <gmarik@gmail.com>
2011-08-30 21:19:42 -0500

    07 commenting

--- 07-ViewControllers/HypnoTime/CurrentTimeViewController.m
+++ 07-ViewControllers/HypnoTime/CurrentTimeViewController.m
@@ -24,9 +24,19 @@
     return self;
 }
 
+// is used to associate controller and XIB file (nibName)
+// so there are 3 options:
+// 1) use nibName to set the XIB name
+// 2) use nil to set default one (controller's name is used)
+// 3) ig none above worked we can still associate it using IB (setting File's owner)
 -(id)initWithNibName:(NSString *)nibName bundle:(NSBundle *) bundle {
     return [self init];
 }
+
+// default implementation is used with XIB, so it should be overriden in that case
+// programmatical view creation requires this overriden
+//-(void)loadView {
+//}
 
 
 -(void)viewDidLoad {
