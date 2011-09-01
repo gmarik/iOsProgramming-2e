d680405ab4ae2ce5afef181d6a504fda62781ba6
gmarik <gmarik@gmail.com>
2011-08-31 22:15:49 -0500

    07 Challenge: view boilerplate

---
+++ 07-ViewControllers/HypnoTime/MapViewController.h
@@ -0,0 +1,16 @@
+//
+//  MapViewController.h
+//  HypnoTime
+//
+//  Created by Marjan Hratson on 8/31/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MapViewController : MapViewController {
+    
+}
+
+@end
