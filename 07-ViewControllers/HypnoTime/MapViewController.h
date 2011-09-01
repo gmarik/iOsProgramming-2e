1c1400d58fc64d38bfe72178250b31e671f5bfaf
gmarik <gmarik@gmail.com>
2011-08-31 22:26:30 -0500

    07 Challenge: comment about runtime error
    - "cannot decode object of class"

--- 07-ViewControllers/HypnoTime/MapViewController.h
+++ 07-ViewControllers/HypnoTime/MapViewController.h
@@ -9,6 +9,10 @@
 #import <UIKit/UIKit.h>
 #import "MapKit/MapKit.h"
 
+// Was getting error
+// cannot decode object of class (MKMapView)
+// SOLUTION: link MapKit.framework in App/Targets/BuildPhases/Link Binary with libraries
+
 @interface MapViewController : UIViewController {
     IBOutlet MKMapView *mapView;    
 }
