d9ed3df81a37689c7894a4f39f9ac9cabc39749d
gmarik <gmarik@gmail.com>
2011-09-01 18:30:44 -0500

    08 initial boilerplate

---
+++ 08-Accelerometer/HypnoAccell/HypnoAccellAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HypnoAccellAppDelegate.h
+//  HypnoAccell
+//
+//  Created by Marjan Hratson on 9/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnoAccellAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
