9daea1d6355f87c33e77773e950e1572d1ec0563
gmarik <gmarik@gmail.com>
2011-09-07 22:31:00 -0500

    09 remove generated boilerplate

--- 09-NotificationAndRotation/HeavyRotation/HeavyViewController.m
+++ 09-NotificationAndRotation/HeavyRotation/HeavyViewController.m
@@ -11,47 +11,9 @@
 
 @implementation HeavyViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)dealloc
-{
-    [super dealloc];
-}
-
-- (void)didReceiveMemoryWarning
-{
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
-}
-
-#pragma mark - View lifecycle
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
+// NOTE:
+// - since default view controller implementation loads NIB file having same name as the
+//   controller it's totally safe to remove all that
+// - also yet there's no instance variables so no reason to owerride defaults
 
 @end
