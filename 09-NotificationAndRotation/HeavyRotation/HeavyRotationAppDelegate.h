d884f65c298a59ef3ee5995ca65adbc17fb3b77d
gmarik <gmarik@gmail.com>
2011-09-07 21:42:11 -0500

    09 initial boilerplate

---
+++ 09-NotificationAndRotation/HeavyRotation/HeavyRotationAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HeavyRotationAppDelegate.h
+//  HeavyRotation
+//
+//  Created by Marjan Hratson on 9/7/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HeavyRotationAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
