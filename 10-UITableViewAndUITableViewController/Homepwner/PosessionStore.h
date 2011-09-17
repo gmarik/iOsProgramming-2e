ef1873235533aed82857f3716379b61744fccd7b
gmarik <gmarik@gmail.com>
2011-09-16 23:35:05 -0500

    10 - add PosessionStore class boilerplate

---
+++ 10-UITableViewAndUITableViewController/Homepwner/PosessionStore.h
@@ -0,0 +1,14 @@
+//
+//  PosessionStore.h
+//  Homepwner
+//
+//  Created by Marjan Hratson on 9/15/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PosessionStore : NSObject
+
+@end
+
---
+++ 10-UITableViewAndUITableViewController/Homepwner/PosessionStore.m
@@ -0,0 +1,14 @@
+//
+//  PosessionStore.m
+//  Homepwner
+//
+//  Created by Marjan Hratson on 9/15/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PosessionStore.h"
+
+@implementation PosessionStore
+
+
+@end
