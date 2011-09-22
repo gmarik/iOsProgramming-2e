ebbc2758deb29d16cca4db3730db1c2be196535e
gmarik <gmarik@gmail.com>
2011-09-21 19:00:13 -0500

    12 adding ItemDetailsViewController

---
+++ 12-UINavigationController/Homepwner/ItemDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ItemDetailsViewController.h
+//  Homepwner
+//
+//  Created by Marjan Hratson on 9/21/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItemDetailsViewController : UIViewController
+
+@end
