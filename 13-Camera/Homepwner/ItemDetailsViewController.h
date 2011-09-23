0399d091a3ad878137c7837e3baf15cc2e3d898f
gmarik <gmarik@gmail.com>
2011-09-22 20:59:49 -0500

    13 copy ch12 as starting point

---
+++ 13-Camera/Homepwner/ItemDetailsViewController.h
@@ -0,0 +1,19 @@
+//
+//  ItemDetailsViewController.h
+//  Homepwner
+//
+//  Created by Marjan Hratson on 9/21/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Posession.h"
+
+@interface ItemDetailsViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UITextField *nameField;
+@property (strong, nonatomic) IBOutlet UITextField *serialField;
+@property (strong, nonatomic) IBOutlet UITextField *valueField;
+@property (strong, nonatomic) IBOutlet UILabel *dateLabel;
+@property  (strong,nonatomic) Posession *posession;
+
+@end
---
+++ 13-Camera/Homepwner/ItemDetailsViewController.m
@@ -0,0 +1,73 @@
+//
+//  ItemDetailsViewController.m
+//  Homepwner
+//
+//  Created by Marjan Hratson on 9/21/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ItemDetailsViewController.h"
+
+@implementation ItemDetailsViewController
+@synthesize nameField;
+@synthesize serialField;
+@synthesize valueField;
+@synthesize dateLabel;
+@synthesize posession;
+
+
+// ViewCallbacks
+
+-(void)viewWillAppear:(BOOL)animated 
+{
+    [super viewWillAppear:animated];
+    [nameField setText:[posession posessionName]];
+    [serialField setText:[posession serialNumber]];
+
+    [valueField setText:[NSString stringWithFormat:@"%d", [posession valueInDollars]]];
+
+    NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
+    [fmt setDateStyle:NSDateFormatterMediumStyle];
+    [fmt setTimeStyle:NSDateFormatterNoStyle];
+    
+    [dateLabel setText:
+     [fmt stringFromDate:[posession dateCreated]]];
+}
+
+-(void)viewWillDisappear:(BOOL)animated 
+{
+    [super viewWillDisappear:animated];
+    
+    // Clear first responder
+    [[self view] endEditing:YES];
+    
+    [posession setPosessionName:[nameField text]];
+    [posession setSerialNumber:[serialField text]];
+    [posession setValueInDollars:[[valueField text] intValue]];
+    
+}
+
+- (void)viewDidUnload {
+//    [setNameField release];
+//    [setSerialField release];
+//    [setValueField release];
+//    [setDateLabel release];
+
+    // reset pointers to not point to 
+    // deallocated objects
+    [self setNameField:nil];
+    [self setSerialField:nil];
+    [self setValueField:nil];
+    [self setDateLabel:nil];
+    [super viewDidUnload];
+}
+
+-(void)dealloc {
+//    [setNameField relese];
+//    [setSerialField release];
+//    [setValueField release];
+//    [setDateLabel release];
+//    [super dealloc];
+}
+
+@end
