58cfb60b846e6674b9d20ec2ffb02583baf7acd8
gmarik <gmarik@gmail.com>
2011-09-24 16:29:13 -0500

    13 add ImageStore.*

---
+++ 13-Camera/Homepwner/ImageStore.h
@@ -0,0 +1,13 @@
+//
+//  ImageStore.h
+//  Homepwner
+//
+//  Created by Marjan Hratson on 9/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageStore : NSObject 
+
+@end
---
+++ 13-Camera/Homepwner/ImageStore.m
@@ -0,0 +1,13 @@
+//
+//  ImageStore.m
+//  Homepwner
+//
+//  Created by Marjan Hratson on 9/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ImageStore.h"
+
+@implementation ImageStore
+
+@end
