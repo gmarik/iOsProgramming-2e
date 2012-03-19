0dfb1beac3ec774d32cfba3dae4f0192075aef96
gmarik <gmarik@gmail.com>
2012-03-18 22:03:40 -0500

    X Load Image resouce with NSBundle

---
+++ X-ObjC/LoadImageFromBundle/LoadImageFromBundle/MHAppDelegate.m
@@ -0,0 +1,39 @@
+//
+//  MHAppDelegate.m
+//  LoadImageFromBundle
+//
+//  Created by Marjan Hratson on 3/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MHAppDelegate.h"
+
+@implementation MHAppDelegate
+
+@synthesize window = _window;
+
+
+// HOWDOI
+// 1) i think I forgot how to associate window created in IB with appDelegate's window
+// 2) how do I scroll image?
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+  
+    // too easy
+    //UIImage *img = [UIImage  imageNamed:@"img.png"];
+    // use Bundle to retrieve resource
+    NSBundle *appBundle = [NSBundle mainBundle];
+    NSString *path = [appBundle pathForResource:@"img" ofType:@"png"];
+    UIImage *img = [[UIImage alloc] initWithContentsOfFile:path]; 
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:img];
+    [imageView setAutoresizesSubviews:YES];
+    [imageView setContentMode:UIViewContentModeCenter];
+    
+    [self.window addSubview:imageView];
+    
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+@end
