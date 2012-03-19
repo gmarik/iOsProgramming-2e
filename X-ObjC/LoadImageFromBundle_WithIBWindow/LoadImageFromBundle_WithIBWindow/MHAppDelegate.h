1acf8165fc0b1a831c77e700a0271482a3068a43
gmarik <gmarik@gmail.com>
2012-03-19 18:55:07 -0500

    X Image from bundle
    - with IB created Window
    - unlike the example with programmatically creating window

---
+++ X-ObjC/LoadImageFromBundle_WithIBWindow/LoadImageFromBundle_WithIBWindow/MHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MHAppDelegate.h
+//  LoadImageFromBundle_WithIBWindow
+//
+//  Created by Marjan Hratson on 3/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
