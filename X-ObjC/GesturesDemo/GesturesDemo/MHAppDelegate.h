8f28f797044f47cf62297ed81bc17e61ea891f3d
gmarik <gmarik@gmail.com>
2012-03-24 18:36:16 -0500

    X Gestures Demo initial commit

---
+++ X-ObjC/GesturesDemo/GesturesDemo/MHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MHAppDelegate.h
+//  GesturesDemo
+//
+//  Created by Marjan Hratson on 3/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
