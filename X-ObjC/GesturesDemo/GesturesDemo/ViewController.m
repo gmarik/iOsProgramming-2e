dcb4dcb7f7b942769173fbda190962c2f29e3beb
gmarik <gmarik@gmail.com>
2012-03-25 01:34:54 -0500

    X Gestures pinch handler

--- X-ObjC/GesturesDemo/GesturesDemo/ViewController.m
+++ X-ObjC/GesturesDemo/GesturesDemo/ViewController.m
@@ -27,6 +27,16 @@
 -(void)loadView {
     self.view = [[View alloc] initWithFrame:CGRectZero];
     [self.view setBackgroundColor:UIColor.whiteColor];
+    
+    UIPinchGestureRecognizer *pinch = [[UIPinchGestureRecognizer alloc] init];
+    [pinch addTarget:self action:@selector(onPinch:)];
+    [self.view addGestureRecognizer:pinch];
+    
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] init];
+    [pan addTarget:self action:@selector(onPan:)];
+    
+    [self.view addGestureRecognizer:pan];
+
 }
 
 - (void)viewDidLoad
@@ -46,4 +56,35 @@
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
+- (void)onPinch:(UIPinchGestureRecognizer *)sender {
+
+    self.view.transform = CGAffineTransformScale(self.view.transform, sender.scale, sender.scale);
+    // THIS IS IMPORTANT
+    // otherwise it scales with geometrical progression
+    sender.scale = 1;
+    
+    if (sender.state == UIGestureRecognizerStateEnded) {
+        NSLog(@"Pinch Ended");
+    } else if (sender.state == UIGestureRecognizerStateBegan) {
+        NSLog(@"Pinch Started");
+    } else {
+        NSLog(@"Pinched");
+    }
+    
+}
+
+- (IBAction)onPan:(UIPanGestureRecognizer *)sender {
+    NSLog(@"Pan");
+/* 
+    CGPoint translate = [sender translationInView:self.view];
+    
+    CGRect newFrame = currentImageFrame;
+    newFrame.origin.x += translate.x;
+    newFrame.origin.y += translate.y;
+    sender.view.frame = newFrame;
+    
+    if (sender.state == UIGestureRecognizerStateEnded)
+        currentImageFrame = newFrame;
+ */
+}
 @end
