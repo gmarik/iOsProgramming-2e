89648ff7d60f572e37d9a5b99777bf9c3c5e578e
gmarik <gmarik@gmail.com>
2012-12-10 07:13:11 -0600

    17 CoreData: generated CoreData Posesion code

---
+++ 17-CoreData/Homepwner/Posession.h
@@ -0,0 +1,25 @@
+//
+//  Posession.h
+//  Homepwner
+//
+//  Created by Maryan Hratson on 2012-12-10.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Posession : NSManagedObject
+
+@property (nonatomic, retain) NSString * posessionName;
+@property (nonatomic, retain) NSString * serialNumber;
+@property (nonatomic, retain) NSNumber * valueInDollars;
+@property (nonatomic, retain) NSDate * dataCreated;
+@property (nonatomic, retain) NSString * imageKey;
+@property (nonatomic, retain) NSData * thumbnailData;
+@property (nonatomic, retain) UNKNOWN_TYPE thumbnail;
+@property (nonatomic, retain) NSNumber * orderingValue;
+@property (nonatomic, retain) NSManagedObject *assetType;
+
+@end
---
+++ 17-CoreData/Homepwner/Posession.m
@@ -0,0 +1,24 @@
+//
+//  Posession.m
+//  Homepwner
+//
+//  Created by Maryan Hratson on 2012-12-10.
+//
+//
+
+#import "Posession.h"
+
+
+@implementation Posession
+
+@dynamic posessionName;
+@dynamic serialNumber;
+@dynamic valueInDollars;
+@dynamic dataCreated;
+@dynamic imageKey;
+@dynamic thumbnailData;
+@dynamic thumbnail;
+@dynamic orderingValue;
+@dynamic assetType;
+
+@end
